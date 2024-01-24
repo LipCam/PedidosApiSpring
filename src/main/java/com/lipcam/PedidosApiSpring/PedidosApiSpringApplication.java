65d037802d2c16d85aaa9d0e3d365c8a5471aa8c
LipCam <lipcam@hotmail.com.br>
2024-01-24 11:52:08 -0300

    Commit inicial

---
+++ src/main/java/com/lipcam/PedidosApiSpring/PedidosApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lipcam.PedidosApiSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lipcam/PedidosApiSpring/PedidosApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lipcam.PedidosApiSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
